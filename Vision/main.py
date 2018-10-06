e7e73fe5c2b92761fb5c16966b0e43caf0262e1c
link00000000 <crandall.logan@gmail.com>
2018-10-06 17:06:18 -0400

    Basic image manipulation

--- Vision/main.py
+++ Vision/main.py
@@ -4,7 +4,13 @@ import numpy as np
 
 stream=urllib.urlopen('http://camera.accidentallycoded.com/video')
 bytes=''
+
+def displayFrame(frame):
+    cv2.imshow("IP Camera", frame)
+
 while True:
+
+    # Get frame from video feed
     bytes+=stream.read(16384)
     a = bytes.find('\xff\xd8')
     b = bytes.find('\xff\xd9')
@@ -12,6 +18,13 @@ while True:
         jpg = bytes[a:b+2]
         bytes= bytes[b+2:]
         frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),cv2.CV_LOAD_IMAGE_COLOR)
-        cv2.imshow('IP Camera', frame)
+        
+        # Manipulate frame
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Display frame to window
+        displayFrame(gray_frame)
+
+        # Close on escape
         if cv2.waitKey(1) ==27:
-            exit(0) 
+            exit(0)
