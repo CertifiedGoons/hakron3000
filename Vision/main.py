23298fdd008bae65de93109dd187420a5afb6d01
link00000000 <crandall.logan@gmail.com>
2018-10-07 02:02:57 -0400

    smal peener

--- Vision/main.py
+++ Vision/main.py
@@ -1,26 +1,42 @@
 import cv2
-import urllib 
 import numpy as np
 
-stream = urllib.urlopen('http://camera.accidentallycoded.com/video')
-bytes = ''
+URL = "http://192.168.1.7:8080/video"
+BLUR_AMOUNT = (5, 5)
+CAM = cv2.VideoCapture(URL)
 
 while True:
+    ret, frame = CAM.read()
+    if ret:
+        dupFrame = cv2.copyMakeBorder(frame, 0, 0, 0, 0, cv2.BORDER_REPLICATE)
 
-    # Get frame from video feed
-    bytes += stream.read(16384)
-    a = bytes.find('\xff\xd8')
-    b = bytes.find('\xff\xd9')
-    if a!=-1 and b!=-1:
-        jpg = bytes[a:b+2]
-        bytes = bytes[b+2:]
-        frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),cv2.CV_LOAD_IMAGE_COLOR)
-        
         # Manipulate frame
         bw = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.blur(bw, BLUR_AMOUNT, 1000)
+        ret, thresh = cv2.threshold(blurred, 127, 255, 0)
+        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours = sorted(contours, key=cv2.contourArea,reverse=True)
+
+        for contour in contours:
+            perimeter = cv2.arcLength(contour, True)
+            approx = cv2.approxPolyDP(contour, .02 * perimeter, True)
+
+            # Draw boxes in separate window
+            box = np.int0(approx)
+            cv2.drawContours(dupFrame, [box], 0, (255, 0, 0), 2)
+
+            # Affine Transform
+            h = np.array([[0, 0], [449, 0], [449, 449], [0, 449]], np.float32)  
+            transform = cv2.getPerspectiveTransform(approx, h)
+            # warp = cv2.warpPerspective(frame, transform, (450, 450))
+
+        cv2.imshow("Boxes", dupFrame)
+
+            # print(type(rect))
+
 
         # Display frame to window
-        image = bw
+        image = frame
         cv2.imshow("IP Camera", image)
 
         # Close on escape
