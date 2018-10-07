f20175f0c8c5e8a9186286755eb7a12c9f90d425
Whaleboye <zd19@zips.uakron.edu>
2018-10-06 23:38:19 -0400

    Big ol breakdown then breakthrough

--- Game/Game.lua
+++ Game/Game.lua
@@ -9,10 +9,13 @@ deck = getObjectFromGUID(deck_guid)
     
 takeParams = {}
 takeParams.position = {x=5, y=0, z=5}
+takeParams.flip = true
 takeParams.index = 1
-takeParams.flip
 
+for i=1, 40 do
 deck.takeObject(takeParams)
+end
 
 end
+
 
---
+++ Game/breakdownthenbreakthrough.lua
@@ -0,0 +1,86 @@
+ca_guid = 'd63d07'
+c2_guid = '3e5ff5'
+c3_guid = 'c319d8'
+c4_guid = 'de7f69'
+c5_guid = 'a95f36'
+c6_guid = '107e63'
+c7_guid = 'fe34d1'
+c8_guid = '9d6a57'
+c9_guid = 'bd8cf2'
+c10_guid = '07fa6a'
+cj_guid = '4156b5'
+cq_guid = 'c8a9cc'
+ck_guid = 'a0984d'
+
+sa_guid = 'cc9386'
+s2_guid = 'a81b8b'
+s3_guid = '6190dc'
+s4_guid = '54654f'
+s5_guid = '86e9c4'
+s6_guid = '4959d8'
+s7_guid = '40a55f'
+s8_guid = '3c415c'
+s9_guid = '46064a'
+s10_guid = '036fbf'
+sj_guid = 'cc1b28'
+sq_guid = '85e29b'
+sk_guid = '5ffd24'
+
+da_guid = '66c505'
+d2_guid = 'e2552a'
+d3_guid = '989e37'
+d4_guid = 'b319f1'
+d5_guid = 'd7fb8f'
+d6_guid = '70dfdf'
+d7_guid = 'cfbf60'
+d8_guid = '70af9c'
+d9_guid = '8a9c01'
+d10_guid = '39770b'
+dj_guid = '8ce2cb'
+dq_guid = '9b4ae6'
+dk_guid = '79ad4c'
+
+ha_guid = 'f8a461'
+h2_guid = 'c5517e'
+h3_guid = 'd228b7'
+h4_guid = '4148ba'
+h5_guid = 'f08e8e'
+h6_guid = '1aa773'
+h7_guid = 'b3310f'
+h8_guid = '569a33'
+h9_guid = 'ab5c84'
+h10_guid = '52cba5'
+hj_guid = 'b3ff44'
+hq_guid = '48f518'
+hk_guid = 'bf7f43'
+
+function onload()
+    ca
+    c2
+    c3
+    c4
+    c5
+    c6
+    c7
+    c8
+    c9
+    c10
+    cj
+    cq
+    ck
+
+    sa
+    s2
+    s3
+    s4
+    s5
+    s6
+    s7
+    s8
+    s9
+    s10
+    sj
+    sq
+    sk
+
+    
