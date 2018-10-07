831caaee7f766241cf38e8e343d908429d4526df
Whaleboye <zd19@zips.uakron.edu>
2018-10-06 21:46:51 -0400

    Pull the first card out from the deck

--- Game/Game.lua
+++ Game/Game.lua
@@ -1,31 +1,18 @@
 
+deck_guid = '6055c0'
+
+
+
 function onload()
-    spawnObject({["type"] = "Deck"})
-    findInRadiusBy({0,0,100}, 100000, function(o) return o.tag=="Card" end, true)
-    cards = findInRadiusBy()
-    for k, v in pairs(cards) do
-        print(k, v)
-    end
-end
+deck = getObjectFromGUID(deck_guid)
 
-function findInRadiusBy(pos, radius, func, debug)
-    local radius = (radius or 1)
-    local objList = Physics.cast({
-        origin=pos, direction={0,1,0}, type=2, size={radius,radius,radius},
-        max_distance=0, debug=(debug or false)
-    })
+    
+takeParams = {}
+takeParams.position = {x=5, y=0, z=5}
+takeParams.index = 1
+takeParams.flip
 
-    local refinedList = {}
-    for _, obj in ipairs(objList) do
-        if func == nil then
-            table.insert(refinedList, obj.hit_object)
-        else
-            if func(obj.hit_object) then
-                table.insert(refinedList, obj.hit_object)
-            end
-        end
-    end
+deck.takeObject(takeParams)
 
-    return refinedList
 end
 
