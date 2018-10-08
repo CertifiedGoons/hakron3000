ed42e86339a28aa34f3b112de5104262fce72817
Rhohio <kad145@zips.uakron.edu>
2018-10-08 10:48:30 -0400

    more documentation; minAreaRect implemented; happy times

--- Vision/main.py
+++ Vision/main.py
@@ -1,12 +1,15 @@
 import cv2
 import numpy as np
 
-
+#url to video stream (Can be device location)
 URL = "http://192.168.1.7:8080/video"
+#global blur amount
 BLUR_AMOUNT = (5, 5)
+#capturing vudeo feed from url
 CAM = cv2.VideoCapture(URL)
 
 while True:
+    #get frame from, and test ret from video feed
     ret, frame = CAM.read()
     if ret:
         dupFrame = cv2.copyMakeBorder(frame, 0, 0, 0, 0, cv2.BORDER_REPLICATE)
@@ -14,11 +17,11 @@ while True:
         # Manipulate frame
         #convert to grayscale
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        #blue the image to reduce noise
+        #blur the image to reduce noise
         blurred = cv2.blur(gray, BLUR_AMOUNT, 1000)
-        #threshold the grayed and blurred image
-        ret, thresh = cv2.threshold(blurred, 127, 255, 0)
-        #form contours based on threshold, ___, ___
+        #use Otsu's thresholding to find best threshold value (from https://is.gd/WChzc5)
+        ret, thresh = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+        #form contours based on threshold, contour retrieval mode, and the contour approximation method
         contours, hierarchy, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         #sort contours by size
         contours = sorted(contours, key=cv2.contourArea,reverse=True)
@@ -26,16 +29,20 @@ while True:
         #form single contours from array of contours
         for contour in contours:
             
-            #approximating contours for drawing
+            #approximating contours for transforming (dunno if we're going to end up using this)
             perimeter = cv2.arcLength(contour, True)
             approx = cv2.approxPolyDP(contour, .02 * perimeter, True)
 
-            # Draw boxes in separate window
-            box = np.int0(approx)
-            cv2.drawContours(dupFrame, [box], 0, (255, 0, 0), 2)
-
+            #minAreRect used for drawing rectangle contours around rotated cards
+            rect = cv2.minAreaRect(contour)
+            #get four corners from minAreaRect in order to drawContours
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)
+            #draw contours!
+            cv2.drawContours(dupFrame,[box],0,(0,0,255),2)
+            
             # Affine Transform
-            h = np.array([[0, 0], [449, 0], [449, 449], [0, 449]], np.float32)  
+            #h = np.array([[0, 0], [449, 0], [449, 449], [0, 449]], np.float32)  
             # transform = cv2.getPerspectiveTransform(approx, h)
             # warp = cv2.warpPerspective(frame, transform, (450, 450))
 
